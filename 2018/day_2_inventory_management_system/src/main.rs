5b48c877b2643715fb2b58d741f11f095c193140
Forest Anderson <forestkzanderson@gmail.com>
2018-12-02 09:37:49 -0500

    d2p22018

--- 2018/day_2_inventory_management_system/src/main.rs
+++ 2018/day_2_inventory_management_system/src/main.rs
@@ -3,44 +3,52 @@ use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 
 fn main() {
-    let checksum = find_checksum();
-    println!("{}", checksum);
+    find_boxes();
 }
 
-fn find_checksum() -> i32 {
+fn find_boxes() {
 
-    let mut twos = 0;
-    let mut threes = 0;
+    let mut strings: Vec<String> = Vec::new();
 
     let file = File::open("src/input.txt").unwrap();
     for line in BufReader::new(file).lines() {
-        let mut chars: HashMap<char, i32> = HashMap::new();
-        let mut this_two = 0;
-        let mut this_three = 0;
-        let this_line = String::from(line.unwrap());
-
-        for this_char in this_line.chars() {
-            if !chars.contains_key(&this_char) {
-                chars.insert(this_char, 1);
-            }
-            else {
-                let count = chars.entry(this_char).or_insert(0);
-                *count += 1;
-            }
-        }
-
-        for (_k, v) in chars {
-            if v == 2 {
-                this_two = 1;
-            }
-            if v == 3 {
-                this_three = 1;
-            }
-        }
-
-        twos += this_two;
-        threes += this_three;
+        strings.push(line.unwrap());
     }
 
-    twos * threes
+    for line in &strings {
+        for compare_line in &strings {
+            let mut chars: HashMap<char, i32> = HashMap::new();
+
+            for this_char in compare_line.chars() {
+                if !chars.contains_key(&this_char) {
+                    chars.insert(this_char, 1);
+                }
+                else {
+                    let count = chars.entry(this_char).or_insert(0);
+                    *count += 1;
+                }
+            }
+
+            let mut extra: bool = true;
+            let mut found: bool = true;
+            for this_char in line.chars() {
+                if !chars.contains_key(&this_char) || chars[&this_char] == 0 {
+                    if extra {
+                        extra = false;
+                    }
+                    else {
+                        found = false;
+                        break
+                    }
+                }
+                else {
+                    let count = chars.entry(this_char).or_insert(0);
+                    *count -= 1;
+                }
+            }
+            if found && !extra {
+                println!("{} {}", line, compare_line);
+            }
+        }
+    }
 }
