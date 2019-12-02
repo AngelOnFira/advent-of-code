9e1b1f89ba7b90447668a2d27b64b23d036e4ee8
Forest Anderson <forestkzanderson@gmail.com>
2019-12-02 10:14:00 -0500

    I missed the bus 4 times to solve this

--- 2019/src/day2.rs
+++ 2019/src/day2.rs
@@ -48,9 +48,45 @@ pub fn solve_part1(input: &str) -> i32 {
     number[0]
 }
 
+#[aoc(day2, part2)]
+pub fn solve_part2(input: &str) -> i32 {
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let mut number = input
+                .split(",")
+                .map(|input| input.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+
+            number[1] = noun;
+            number[2] = verb;
+            let mut index = 0;
+            while index + 4 < number.len() {
+                let opcode = number[index];
+                let read_loc1 = number[index + 1] as usize;
+                let read_loc2 = number[index + 2] as usize;
+                let out_loc = number[index + 3] as usize;
+                match opcode {
+                    1 => number[out_loc] = number[read_loc1] + number[read_loc2],
+                    2 => number[out_loc] = number[read_loc1] * number[read_loc2],
+                    99 => index = number.len(),
+                    _ => unreachable!(),
+                }
+                index += 4;
+            }
+            if number[0] == 19690720 {
+                println!("{} {}", noun, verb);
+                return 100 * noun + verb;
+            }
+        }
+    }
+
+    unreachable!();
+}
+
 #[cfg(test)]
 mod tests {
     use super::solve_part1 as part1;
+    use super::solve_part2 as part2;
 
     #[test]
     fn sample1() {
@@ -65,5 +101,10 @@ mod tests {
     #[test]
     fn sample3() {
         assert_eq!(part1("1,9,10,3,2,3,11,0,99,30,40,50"), 3500);
+    }
+
+    #[test]
+    fn sample4() {
+        assert_eq!(part2("1, 0, 0, 0, 99"), 1202);
     }
 }
