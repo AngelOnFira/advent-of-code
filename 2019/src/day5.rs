bf1d8b4d0f420d20cf4cb751154c2ea3645cdfa8
Forest Anderson <forestkzanderson@gmail.com>
2019-12-05 20:50:35 -0500

    Day 5 complete

--- 2019/src/day5.rs
+++ 2019/src/day5.rs
@@ -3,7 +3,7 @@ struct State {
     instructions: Vec<i32>,
 }
 
-#[aoc(day5, part1)]
+//#[aoc(day5, part1)]
 pub fn solve_part1(input: &str) -> i32 {
     let mut state = State {
         instruction_pointer: 0,
@@ -34,12 +34,12 @@ pub fn solve_part1(input: &str) -> i32 {
             opcode = opcode_int;
             parameters = [0, 0, 0].to_vec();
         }
-        //println!("{:?} {}", parameters, opcode);
+        println!("{:?} {}", parameters, opcode);
         match opcode {
             1 => opcode_1(&mut state, parameters),
             2 => opcode_2(&mut state, parameters),
             3 => opcode_3(&mut state),
-            4 => println!("{}", opcode_4(&mut state)),
+            4 => println!("{}", opcode_4(&mut state, parameters)),
             99 => return -1,
             _ => unreachable!(),
         }
@@ -76,26 +76,106 @@ fn opcode_2(state: &mut State, parameter_modes: Vec<i32>) {
 
 fn opcode_3(state: &mut State) {
     set_position(
-        state, 0, 1, 1, // This is hardcoded as part of the problem input
+        state, 0, 1, 5, // This is hardcoded as part of the problem input
     );
     state.instruction_pointer += 2;
 }
 
-fn opcode_4(state: &mut State) -> i32 {
-    let value = get_position(state, 0, 1);
+fn opcode_4(state: &mut State, parameter_modes: Vec<i32>) -> i32 {
+    let value = get_position(state, parameter_modes[0], 1);
     state.instruction_pointer += 2;
     value
 }
 
+fn opcode_5(state: &mut State, parameter_modes: Vec<i32>) {
+    if get_position(state, parameter_modes[0], 1) != 0 {
+        state.instruction_pointer = get_position(state, parameter_modes[1], 2) as usize;
+    }
+    else {
+        state.instruction_pointer += 3;
+    }
+}
+
+fn opcode_6(state: &mut State, parameter_modes: Vec<i32>) {
+    if get_position(state, parameter_modes[0], 1) == 0 {
+        state.instruction_pointer = get_position(state, parameter_modes[1], 2) as usize;
+    }
+    else {
+        state.instruction_pointer += 3;
+    }
+}
+
+fn opcode_7(state: &mut State, parameter_modes: Vec<i32>) {
+    if get_position(state, parameter_modes[0], 1) < get_position(state, parameter_modes[1], 2) {
+        set_position(state, parameter_modes[2], 3, 1);
+    }
+    else {
+        set_position(state, parameter_modes[2], 3, 0);
+    }
+    state.instruction_pointer += 4;
+}
+
+fn opcode_8(state: &mut State, parameter_modes: Vec<i32>) {
+    if get_position(state, parameter_modes[0], 1) == get_position(state, parameter_modes[1], 2) {
+        set_position(state, parameter_modes[2], 3, 1);
+    }
+    else {
+        set_position(state, parameter_modes[2], 3, 0);
+    }
+    state.instruction_pointer += 4;
+}
+
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &str) -> i32 {
-    12
+    let mut state = State {
+        instruction_pointer: 0,
+        instructions: Default::default(),
+    };
+
+    state.instructions = input
+        .split(",")
+        .map(|input| input.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    loop {
+        let opcode_int = state.instructions[state.instruction_pointer];
+
+        let opcode_string = opcode_int.to_string();
+
+        let opcode;
+        let mut parameters: Vec<i32> = Vec::new();
+
+        if opcode_string.len() > 1 {
+            opcode = opcode_string[opcode_string.len() - 2..]
+                .parse::<i32>()
+                .unwrap();
+            for exp in 0..3 {
+                parameters.push((opcode_int % 10i32.pow(3 + exp)) / 10i32.pow(2 + exp));
+            }
+        } else {
+            opcode = opcode_int;
+            parameters = [0, 0, 0].to_vec();
+        }
+        println!("{:?} op {} loc {}", parameters, opcode, state.instruction_pointer + 1);
+        match opcode {
+            1 => opcode_1(&mut state, parameters),
+            2 => opcode_2(&mut state, parameters),
+            3 => opcode_3(&mut state),
+            4 => println!("{}", opcode_4(&mut state, parameters)),
+            5 => opcode_5(&mut state, parameters),
+            6 => opcode_6(&mut state, parameters),
+            7 => opcode_7(&mut state, parameters),
+            8 => opcode_8(&mut state, parameters),
+            99 => return -1,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::solve_part1 as part1;
-    //use super::solve_part2 as part2;
+    use super::solve_part2 as part2;
 
     #[test]
     fn sample511() {
@@ -110,5 +190,10 @@ mod tests {
     #[test]
     fn sample513() {
         assert_eq!(part1("1101,100,-1,6,4,6,0"), 99);
+    }
+
+    #[test]
+    fn sample521() {
+        assert_eq!(part2("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"), 12321);
     }
 }
