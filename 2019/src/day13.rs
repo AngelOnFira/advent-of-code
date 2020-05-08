7250dfe231739955517f9dcbb405ae70d0525b47
Forest <forestkzanderson@gmail.com>
2020-05-07 21:14:05 -0400

    Day 13 pt 1

---
+++ 2019/src/day13.rs
@@ -0,0 +1,98 @@
+use super::intcode::intcode::{call_intcode, State};
+use std::collections::HashMap;
+
+#[aoc(day13, part1)]
+pub fn solve_part1(input: &str) -> i128 {
+    let mut panels: HashMap<String, i8> = HashMap::new();
+
+    let mut state = State {
+        instruction_pointer: 0,
+        relative_pointer: 0,
+        instructions: Default::default(),
+        input: Vec::new(),
+        output: Vec::new(),
+        memory: HashMap::new(),
+    };
+
+    state.instructions = input
+        .split(",")
+        .map(|input| input.parse::<i128>().unwrap())
+        .collect::<Vec<i128>>();
+
+    // state.input.push(curr_color as i128);
+    state.memory.insert("0".to_string(), 2);
+
+    let mut block_count = 0;
+    let mut x;
+
+    loop {
+        x = call_intcode(&mut state, true);
+        if x == -1 {
+            return block_count;
+        }
+
+        let _ = call_intcode(&mut state, true);
+
+        if call_intcode(&mut state, true) == 2 {
+            block_count += 1;
+        }
+
+    }
+}
+
+#[aoc(day13, part2)]
+pub fn solve_part2(input: &str) -> i128 {
+    let mut panels: HashMap<String, i8> = HashMap::new();
+
+    let mut state = State {
+        instruction_pointer: 0,
+        relative_pointer: 0,
+        instructions: Default::default(),
+        input: Vec::new(),
+        output: Vec::new(),
+        memory: HashMap::new(),
+    };
+
+    state.instructions = input
+        .split(",")
+        .map(|input| input.parse::<i128>().unwrap())
+        .collect::<Vec<i128>>();
+
+    state.input.push(-1);
+    state.input.push(0);
+    state.input.push(0);
+    state.input.push(0);
+    state.input.push(0);
+    state.input.push(0);
+    state.input.push(0);
+    state.input.push(0);
+
+    state.memory.insert("0".to_string(), 2);
+
+    let mut block_count = 0;
+    let mut x;
+
+    loop {
+        x = call_intcode(&mut state, true);
+        if x == -1 {
+            return block_count;
+        }
+
+        let y = call_intcode(&mut state, true);
+
+        match call_intcode(&mut state, true) {
+            2 => block_count += 1,
+            i => {
+                if x == -1 && y == 0 {
+                    println!("{}", i);
+                }
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::solve_part1 as part1;
+    // use super::solve_part2 as part2;
+}
--- 2019/src/lib.rs
+++ 2019/src/lib.rs
@@ -17,6 +17,7 @@ pub mod day9;
 pub mod day10;
 pub mod day11;
 pub mod day12;
+pub mod day13;
 pub mod day16;
 pub mod day24;
 //pub mod day22;
