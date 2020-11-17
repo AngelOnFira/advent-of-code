bc0df5005569bc21818b6c63c3be7567668d743f
Forest Anderson <forestkzanderson@gmail.com>
2020-11-17 09:50:25 -0500

    Day 12 pt 1

--- 2019/src/day12.rs
+++ 2019/src/day12.rs
@@ -1,58 +1,9 @@
-use specs::prelude::*;
-// use specs::DispatcherBuilder;
-use specs::{Component, VecStorage};
-use specs::{Join, System};
-use specs::{World, WorldExt};
-// use std::error::Error;
-
 use regex::Regex;
-
-#[derive(Component, Debug)]
-#[storage(VecStorage)]
-struct Position {
-    x: i32,
-    y: i32,
-    z: i32,
-}
-
-#[derive(Component, Debug)]
-#[storage(VecStorage)]
-struct Velocity {
-    x: i32,
-    y: i32,
-    z: i32,
-}
-
-struct CalculateGravity;
-
-impl<'a> System<'a> for CalculateGravity {
-    type SystemData = ReadStorage<'a, Position>;
-
-    fn run(&mut self, position: Self::SystemData) {
-        for position in position.join() {
-            println!("Hello, {:?}", &position);
-        }
-    }
-}
-
-struct UpdatePos;
-
-impl<'a> System<'a> for UpdatePos {
-    type SystemData = (ReadStorage<'a, Velocity>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (vel, mut pos): Self::SystemData) {
-        for (vel, pos) in (&vel, &mut pos).join() {
-            pos.x += vel.x;
-            pos.y += vel.y;
-            pos.z += vel.z;
-        }
-    }
-}
+use std::collections::HashMap;
 
 #[aoc_generator(day12)]
 fn parse_input_day1(input: &str) -> Vec<Vec<i32>> {
-    let re = Regex::new(r"<x=(?P<x>.*), y=(?P<y>.*), z=(?P<z>.*)").unwrap();
-    println!("tesatest");
+    let re = Regex::new(r"<x=(?P<x>.*), y=(?P<y>.*), z=(?P<z>.*)>").unwrap();
 
     input
         .lines()
@@ -69,78 +20,110 @@ fn parse_input_day1(input: &str) -> Vec<Vec<i32>> {
 
 #[aoc(day12, part1)]
 pub fn solve_part1(input: &[Vec<i32>]) -> i32 {
-    let mut world = World::new();
-    world.register::<Position>();
-    world.register::<Velocity>();
-
+    println!("{:?}", input);
     let mut moons = Vec::new();
+    let mut state_map = HashMap::new();
 
     for moon in input {
-        moons.push(
-            // world
-            //     .create_entity()
-            //     .with(Position {
-            //         x: moon[0],
-            //         y: moon[1],
-            //         z: moon[2],
-            //     })
-            //     .build(),
-            vec![moon[0], moon[1], moon[2]],
-        );
+        moons.push(vec![moon[0], moon[1], moon[2]]);
     }
 
-    // let mut dispatcher = DispatcherBuilder::new()
-    //     .with(CalculateGravity, "hello_world", &[])
-    //     .with(UpdatePos, "update_pos", &["hello_world"])
-    //     .build();
+    let mut velocities: Vec<Vec<i32>> = Vec::new();
 
-    // let mut calculate_gravity = CalculateGravity;
-    // for i in 0..1000 {
-    //     calculate_gravity.run_now(&world);
-    //     world.maintain();
-    // }
+    for _ in 0..(moons.len()) {
+        velocities.push(vec![0, 0, 0]);
+    }
 
-    for j in 0..10 {
-        let new_vels: Vec<&[i32]> = Vec::new();
+    let mut j = 0;
 
-        for moon in moons.clone() {
-            let mut vel = [0; 3];
+    loop {
+        if j % 1_000_000 == 0 {
+            println!("{}", j)
+        }
+        for (moon_idx, moon) in moons.clone().iter().enumerate() {
             for moon2 in moons.clone() {
-                if moon != moon2 {
+                if *moon != moon2 {
                     for i in 0..3 {
                         if moon[i] < moon2[i] {
-                            vel[i] = 1;
-                        } else {
-                            vel[i] = -1;
+                            velocities[moon_idx][i] += 1;
+                        } else if moon[i] > moon2[i] {
+                            velocities[moon_idx][i] -= 1;
                         }
                     }
                 }
             }
         }
 
-        println!("After {} steps", j);
+        // println!("After {} steps", j + 1);
         for moon_index in 0..moons.len() {
-            println!(
-                "pos=<x= {}, y= {}, z= {}>, vel=<x= {}, y= {}, z= {}>",
-                moons[moon_index][0],
-                moons[moon_index][1],
-                moons[moon_index][2],
-                new_vels[moon_index][0],
-                new_vels[moon_index][1],
-                new_vels[moon_index][2],
-            );
-
             for i in 0..3 {
-                moons[moon_index][i] += new_vels[moon_index][i];
+                moons[moon_index][i] += velocities[moon_index][i];
             }
-        }
-    }
 
-    12
+            // println!(
+            //     "pos=<x= {}, y= {}, z= {}>, vel=<x= {}, y= {}, z= {}>",
+            //     moons[moon_index][0],
+            //     moons[moon_index][1],
+            //     moons[moon_index][2],
+            //     velocities[moon_index][0],
+            //     velocities[moon_index][1],
+            //     velocities[moon_index][2],
+            // );
+        }
+        // println!(
+        //     "Total energy is {}",
+        //     moons
+        //         .iter()
+        //         .zip(velocities.iter())
+        //         .map(|(pos, vel)| pos.iter().map(|x| x.abs()).sum::<i32>()
+        //             * vel.iter().map(|x| x.abs()).sum::<i32>())
+        //         .sum::<i32>()
+        // );
+
+        if let Some(old) = state_map.insert(moons.clone(), j) {
+            return j - old;
+        }
+        j += 1;
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    // use super::solve_part1 as part1;
+    use super::parse_input_day1 as gen1;
+    use super::solve_part1 as part1;
     // use super::solve_part2 as part2;
+
+    #[test]
+    fn sample911() {
+        println!(
+            "{:?}",
+            gen1("<x=-8, y=-10, z=0>\n<x=5, y=5, z=10>\n<x=2, y=-7, z=3>\n<x=9, y=-8, z=-3>")
+        );
+    }
+
+    #[test]
+    fn sample1211() {
+        assert_eq!(
+            12,
+            part1(&vec![
+                vec![-1, 0, 2],
+                vec![2, -10, -7],
+                vec![4, -8, 8],
+                vec![3, 5, -1]
+            ])
+        );
+    }
+
+    #[test]
+    fn sample1212() {
+        assert_eq!(
+            12,
+            part1(&vec![
+                vec![-8, -10, 0],
+                vec![5, 5, 10],
+                vec![2, -7, 3],
+                vec![9, -8, -3],
+            ])
+        );
+    }
 }
