028cb60a04ec939bf310f9b9812619b2127cbd81
Forest Anderson <forestkzanderson@gmail.com>
2020-11-29 22:23:44 -0500

    pt 2

--- 2015/src/day1.rs
+++ 2015/src/day1.rs
@@ -11,6 +11,22 @@ pub fn solve_part1(input: &str) -> i32 {
     count
 }
 
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &str) -> i32 {
+    let mut count = 0;
+    for (i, character) in input.chars().enumerate() {
+        match character {
+            '(' => count += 1,
+            ')' => count -= 1,
+            _ => unreachable!(),
+        };
+        if count < 0 {
+            return (i + 1) as i32;
+        }
+    }
+    unreachable!();
+}
+
 #[cfg(test)]
 mod tests {
     use super::solve_part1 as part1;
