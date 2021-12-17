d501ae077c07d4359c6be0f3bd4003df7afeecde
Forest Anderson <forestkzanderson@gmail.com>
2021-12-17 00:41:54 -0500

    days

---
+++ 2021/src/day17.rs
@@ -0,0 +1,98 @@
+use regex::Regex;
+
+pub struct Instruction {}
+
+#[aoc_generator(day17)]
+pub fn input_generator(input: &str) -> i32 {
+    //target area: x=85..145, y=-163..-108
+    1
+}
+
+#[aoc(day17, part1)]
+pub fn solve_part1(input: &i32) -> i32 {
+    let x1 = 85;
+    let x2 = 145;
+    let y1 = -163;
+    let y2 = -108;
+
+    let mut max_y = 0;
+
+    for x in -100..1000 {
+        for y in -100..1000 {
+            // dbg!((x, y));
+            let mut curr_pos = (0, 0);
+            let mut curr_vel = (x, y);
+            let mut this_max_y = 0;
+            for _ in 0..1000 {
+                curr_pos.0 += curr_vel.0;
+                curr_pos.1 += curr_vel.1;
+
+                // Due to drag, the probe's x velocity changes by 1 toward the
+                // value 0; that is, it decreases by 1 if it is greater than 0,
+                // increases by 1 if it is less than 0, or does not change if it
+                // is already 0.
+
+                if curr_vel.0 < 0 {
+                    curr_vel.0 += 1;
+                } else if curr_vel.0 > 0 {
+                    curr_vel.0 -= 1;
+                }
+
+                curr_vel.1 -= 1;
+
+                if curr_pos.1 > this_max_y {
+                    // dbg!(curr_pos);
+                    this_max_y = curr_pos.1;
+                }
+
+                // Check if curr_pos is in the target area
+                if curr_pos.0 >= x1 && curr_pos.0 <= x2 && curr_pos.1 >= y1 && curr_pos.1 <= y2 {
+                    // dbg!(this_max_y);
+                    if this_max_y > max_y {
+                        max_y = this_max_y;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    max_y
+}
+
+#[aoc(day17, part2)]
+pub fn solve_part2(input: &i32) -> i32 {
+    let x1 = 85;
+    let x2 = 145;
+    let y1 = -163;
+    let y2 = -108;
+
+    let mut inside_count = 0;
+
+    for x in -1000..1000 {
+        for y in -1000..1000 {
+            let mut curr_pos = (0, 0);
+            let mut curr_vel = (x, y);
+            for _ in 0..1000 {
+                curr_pos.0 += curr_vel.0;
+                curr_pos.1 += curr_vel.1;
+
+                if curr_vel.0 < 0 {
+                    curr_vel.0 += 1;
+                } else if curr_vel.0 > 0 {
+                    curr_vel.0 -= 1;
+                }
+
+                curr_vel.1 -= 1;
+
+                // Check if curr_pos is in the target area
+                if curr_pos.0 >= x1 && curr_pos.0 <= x2 && curr_pos.1 >= y1 && curr_pos.1 <= y2 {
+                    inside_count += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    inside_count
+}
--- 2021/src/lib.rs
+++ 2021/src/lib.rs
@@ -4,6 +4,14 @@ extern crate aoc_runner;
 extern crate aoc_runner_derive;
 
 pub mod day1;
+pub mod day10;
+pub mod day11;
+pub mod day12;
+pub mod day13;
+pub mod day14;
+pub mod day15;
+pub mod day16;
+pub mod day17;
 pub mod day2;
 pub mod day4;
 pub mod day5;
@@ -11,11 +19,5 @@ pub mod day6;
 pub mod day7;
 pub mod day8;
 pub mod day9;
-pub mod day10;
-pub mod day11;
-pub mod day12;
-pub mod day13;
-pub mod day14;
-pub mod day16;
 
 aoc_lib! { year = 2021 }
