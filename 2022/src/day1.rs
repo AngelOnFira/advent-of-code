fb227a6aefebad54f64e235bcc4297d22980605b
Forest Anderson <forestkzanderson@gmail.com>
2022-12-01 00:34:06 -0500

    Improved with iterators

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -1,42 +1,45 @@
+use itertools::Itertools;
+
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &str) -> i32 {
-    // Read and add up each line and add up numbers until there is a blank line.
-    // Return the max
-    let mut curr_counter = 0;
-    let mut max_counter = 0;
-    for line in input.lines() {
-        if line == "" {
-            if curr_counter > max_counter {
-                max_counter = curr_counter;
+*input
+    .lines()
+    .fold((Vec::new(), 0), |(mut acc, counter), line| {
+        match line.parse::<i32>() {
+            Ok(num) => (acc, counter + num),
+            Err(_) => {
+                acc.push(counter);
+                (acc, 0)
             }
-            dbg!(curr_counter);
-            curr_counter = 0;
-        } else {
-            curr_counter += line.parse::<i32>().unwrap();
         }
-    }
-    max_counter
+    })
+    .0
+    .iter()
+    .sorted()
+    .rev()
+    .next()
+    .unwrap()
 }
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &str) -> i32 {
-    // Do the same as above but sum the top 3
-    let mut curr_counter = 0;
-    let mut sums = Vec::new();
-    for line in input.lines() {
-        if line == "" {
-            sums.push(curr_counter);
-            curr_counter = 0;
-        } else {
-            curr_counter += line.parse::<i32>().unwrap();
-        }
-    }
-    sums.sort();
-    
-    
-    // Add up the top 3
-    sums[sums.len() - 1] + sums[sums.len() - 2] + sums[sums.len() - 3]
-
+    input
+        .lines()
+        .fold((Vec::new(), 0), |(mut acc, counter), line| {
+            match line.parse::<i32>() {
+                Ok(num) => (acc, counter + num),
+                Err(_) => {
+                    acc.push(counter);
+                    (acc, 0)
+                }
+            }
+        })
+        .0
+        .iter()
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<i32>()
 }
 
 #[cfg(test)]
