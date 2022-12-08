15392b240518aa57a1bfda5e880810b0e91593d0
Forest Anderson <forestkzanderson@gmail.com>
2022-12-08 00:16:31 -0500

    brain on fire today

---
+++ 2022/template.rs
@@ -0,0 +1,29 @@
+use std::{
+    collections::{HashMap, HashSet},
+    iter::FromIterator,
+};
+
+use itertools::Itertools;
+use regex::Regex;
+
+#[aoc_generator(day8)]
+fn parse_input_day8(input: &str) -> InputType {
+    // Chars
+    // input.chars().collect()
+
+    // Map to ints
+    // input.lines().map(|x| x.parse::<i32>().unwrap()).collect()
+
+    // Regex
+    input.lines().map(|x| {}).collect()
+}
+
+#[aoc(day8, part1)]
+pub fn solve_part1(input: &InputType) -> i32 {
+    0
+}
+
+#[aoc(day8, part2)]
+pub fn solve_part2(input: &InputType) -> i32 {
+    0
+}
