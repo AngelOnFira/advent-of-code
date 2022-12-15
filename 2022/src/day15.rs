9c5dc51dd5ef8f73914ffc1f5c1ac2fb029e69d0
Forest Anderson <forestkzanderson@gmail.com>
2022-12-15 01:07:58 -0500

    checkpoitn

--- 2022/src/day15.rs
+++ 2022/src/day15.rs
@@ -28,6 +28,7 @@ fn parse_input_day15(input: &str) -> InputType {
 
 #[aoc(day15, part1)]
 pub fn solve_part1(input: &InputType) -> i32 {
+    return 0;
     // Get the range of each sensor
     let ranges: Vec<((i32, i32), i32)> = input
         .iter()
@@ -74,43 +75,65 @@ pub fn solve_part2(input: &InputType) -> i32 {
         })
         .collect();
 
-    let beacon_positions = input
-        .iter()
-        .map(|((x, y), (bx, by))| (*bx, *by))
-        .collect::<HashSet<_>>();
 
-    let y = 2_000_000;
 
     let x_min = 0;
     let y_min = 0;
-    let x_max = 4_000_000;
-    let y_max = 4_000_000;
+    let x_max = 4000000;
+    let y_max = 4000000;
 
-    let range = 100_000_000;
+    // Start by searching in a box around the sensor
 
-    (x_min..=x_max)
+    // Go over each sensor, draw a diamond shape around it at distance +
+    // 1
+    let potential_places: HashSet<_> = ranges
+        .clone()
         .into_par_iter()
-        .map(|x| {
-            (y_min..=y_max)
-                .into_iter()
-                .filter(|y| {
-                    // If we're within range of any sensor, but we're not at the
-                    // location of a beacon, we're valid
-                    if beacon_positions.contains(&(x, *y)) {
-                        return false;
-                    }
+        .map(|r| {
+            let mut potential_places = HashSet::new();
 
-                    if ranges.iter().any(|((x1, y1), dist)| {
-                        let dist1 = (x1 - x).abs() + (y1 - y).abs();
-                        dist1 <= *dist
-                    }) {
-                        return false;
-                    }
-                    // Print the position
-                    println!("{} {}", x, y);
-                    return true;
-                })
-                .count() as i32
+            let left_start = (r.0 .0 - r.1 - 1, r.0 .1);
+            let right_start = (r.0 .0 + r.1 + 1, r.0 .1);
+
+            // Repeat for a while
+            for i in 0..=r.1 + 2 {
+                // Left side
+                potential_places.insert((left_start.0 + i, left_start.1 + i));
+                potential_places.insert((left_start.0 + i, left_start.1 - i));
+
+                // Right side
+                potential_places.insert((right_start.0 - i, right_start.1 + i));
+                potential_places.insert((right_start.0 - i, right_start.1 - i));
+            }
+
+            potential_places
         })
-        .count() as i32
+        .flatten()
+        .collect();
+
+    println!("Potential places: {}", potential_places.len());
+
+    let place = potential_places
+        .clone()
+        .into_par_iter()
+        .find_first(|(x_check, y_check)| {
+            // Skip if we're outside the range
+            if x_check < &x_min || x_check > &x_max || y_check < &y_min || y_check > &y_max {
+                return false;
+            }
+
+            // Check if this spot is within the range of any other
+            // sensor
+            if ranges.iter().any(|((x2, y2), dist2)| {
+                let dist3 = (x2 - x_check).abs() + (y2 - y_check).abs();
+                dist3 <= *dist2
+            }) {
+                return false;
+            }
+            // Print the position
+            true
+        })
+        .unwrap();
+
+    place.0 * 4_000_000 + place.1
 }
